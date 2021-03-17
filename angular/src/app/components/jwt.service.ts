e40d21e89099105acdf2a2616e7f4a9e50199658
hoangluu2001 <hoangthehoaluu@gmail.com>
2021-03-17 16:23:14 +0700

    login admin

--- angular/src/app/components/jwt.service.ts
+++ angular/src/app/components/jwt.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+export class User {
+  // tslint:disable-next-line:ban-types
+  name!: String;
+  // tslint:disable-next-line:ban-types
+  email!: String;
+  // tslint:disable-next-line:ban-types
+  password!: String;
+  // tslint:disable-next-line:ban-types variable-name
+  password_confirmation!: String;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class JwtService {
-
-  constructor() { }
+  private baseUrl = 'http://127.0.0.1:8001/api/auth/login';
+  constructor(private http: HttpClient) { }
+  login(email: string, password: string): Observable<any> {
+    const reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    const data = {
+      email,
+      password
+    };
+    return this.http.post(`${this.baseUrl}`, data, { headers: reqHeader, responseType: 'json' });
+  }
+  signUp(user: User): Observable<any> {
+    return this.http.post('http://127.0.0.1:8000/api/auth/register', user);
+  }
+  logIn(user: User): Observable<any> {
+    return this.http.post<any>('http://127.0.0.1:8001/api/auth/login', user);
+  }
+  profile(): Observable<any> {
+    return this.http.get('http://127.0.0.1:8000/api/auth/user-profile');
+  }
+  // req-password-reset
+  // tslint:disable-next-line:typedef
+  reqPasswordReset(data: any) {
+    return this.http.post('http://127.0.0.1:8000/api/auth/req-password-reset', data);
+  }
+  // update password
+  // tslint:disable-next-line:typedef
+  updatePassword(data: any) {
+    return this.http.post('http://127.0.0.1:8000/api/auth/update-password', data);
+  }
 }
--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+import {JwtService} from '../jwt.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  signinForm: any = FormGroup;
+  errors: any = null;
+  email = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public fb: FormBuilder,
+    public jwtService: JwtService,
+  ) {
+    this.signinForm = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
 
   ngOnInit(): void {
+    this.onSubmit();
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.jwtService.logIn(this.signinForm.value).subscribe(
+      (data: { token: string; }) => {
+        localStorage.setItem('AccessToken', data.token);
+        this.router.navigate(['/admin']);
+      }, (error: any) => {
+        console.log(error);
+      }
+    );
   }
 
 }
