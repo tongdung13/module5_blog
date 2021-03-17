7ec64d8e990e331942b5c1428d842e8c3f432a56
tongdung13 <tongvanthienvu@gmail.com>
2021-03-17 11:07:44 +0700

    login

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except', ['login', 'register']]);
+    }
+
+    public function login(Request $request)
+    {
+        $req = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if ($req->fails()) {
+            return response()->json($req->errors(), 422);
+        }
+
+        if (! $token = Auth::attempt($req->validated())) {
+            return response()->json(['Auth error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createToken($token);
+    }
+
+
+    public function createToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' > Auth::factory()->getTTL() * 60,
+            'user' => Auth::user()
+        ]);
+    }
 }
